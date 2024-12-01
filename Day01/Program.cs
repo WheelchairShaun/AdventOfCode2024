bf1115d062ff5ed64ac3c2f7dc1bc460ece0633d
Shaun Barnes <shaun.w.barnes@gmail.com>
2024-12-01 10:02:14 -0500

    Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,39 @@
+﻿using Helpers;
+
+string projectPath = @"C:\Dev\AdventOfCode2024\Day01\";
+
+string testInput =
+	"""
+	3   4
+	4   3
+	2   5
+	1   3
+	3   9
+	3   3
+	""";
+
+long distances = 0L;
+
+List<int> left = new List<int>();
+List<int> right = new List<int>();
+
+var lines = Input.ReadInputFromFile(projectPath + "Locations.txt", testInput);
+
+foreach (string line in lines)
+{
+	var locations = line.Split("  ");
+	left.Add(int.Parse(locations[0]));
+	right.Add(int.Parse(locations[1]));
+}
+
+left.Sort();
+right.Sort();
+
+for (int i = 0; i < left.Count; i++)
+{
+	distances += long.Abs(left[i] - right[i]);
+}
+
+Console.WriteLine($"The total distance between the left list and the right list is {distances}");
+
+Console.ReadLine();
---
+++ Helpers/Input.cs
@@ -0,0 +1,16 @@
+﻿namespace Helpers
+{
+	public static class Input
+	{
+		public static string[] ReadInputFromFile(string path, string testInput)
+		{
+			if (File.Exists(path))
+			{
+				return File.ReadAllLines(path);
+			}
+
+			var lines = testInput.Split(System.Environment.NewLine);
+			return lines;
+		}
+	}
+}
