689046a4ebfc29be8796ffd3b422babb2b77eeae
Shaun Barnes <shaun.w.barnes@gmail.com>
2024-12-02 12:55:45 -0500

    Day 2 part 1

---
+++ Day02/Program.cs
@@ -0,0 +1,28 @@
+﻿using Day02;
+using Helpers;
+
+string testInput =
+	"""
+	7 6 4 2 1
+	1 2 7 8 9
+	9 7 6 2 1
+	1 3 2 4 5
+	8 6 4 4 1
+	1 3 6 7 9
+	""";
+
+var reports = new List<Report>();
+
+var lines = Input.ReadInputFromFile(@"C:\Dev\AdventOfCode2024\Day02\Reports.txt", testInput);
+
+foreach (var line in lines)
+{
+	var input = line.Split(' ').Select(int.Parse).ToArray();
+	reports.Add(new Report(input));
+}
+
+var safe = reports.FindAll(r => r.isSafe() == true);
+
+Console.WriteLine($"{safe.Count} reports are safe.");
+
+Console.ReadLine();
---
+++ Day02/Report.cs
@@ -0,0 +1,25 @@
+﻿namespace Day02
+{
+	public class Report
+	{
+		public int[] levels;
+		private int[] changes;
+
+		public Report(int[] levels)
+		{
+			this.levels = levels;
+			changes = new int[levels.Length - 1];
+
+			for (int i = 1; i < levels.Length; i++)
+			{
+				changes[i - 1] = levels[i - 1] - levels[i];
+			}
+		}
+
+		public bool isSafe()
+		{
+			return changes.All(x => (x > 0) || changes.All(x => x < 0))
+				&& changes.All(x => int.Abs(x) >= 1 && int.Abs(x) <= 3);
+		}
+	}
+}
