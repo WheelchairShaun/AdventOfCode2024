ff230277fb96efda74647b96a81116439d4b58df
Shaun Barnes <shaun.w.barnes@gmail.com>
2024-12-03 16:22:34 -0500

    Day 3 Part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -4,11 +4,11 @@ using System.Text.RegularExpressions;
 var results = 0L;
 
 string testInput =
-	"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+	"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
-var input = Input.GetInputFromFileAsString(@"C:\Dev\AdventOfCode2024\Day03\Program1.txt", testInput);
+var input = Input.GetInputFromFileAsString(@"C:\Dev\AdventOfCode2024\Day03\Program.txt", testInput);
 
-string pattern = "((mul)\\((\\d+),(\\d+)\\))";
+string pattern = @"((mul)\((\d+),(\d+)\))";
 
 var matches = Regex.Matches(input, pattern);
 
@@ -18,6 +18,40 @@ foreach (Match match in matches)
 	long b = long.Parse(match.Groups[4].Value);
 
 	results += a * b;
+}
+
+Console.WriteLine($"The results of the multiplications are {results}");
+
+// Part 2
+
+results = 0L;
+
+pattern = @"((don't\(\)|do\(\))|(mul)\((\d+),(\d+)\))";
+
+matches = Regex.Matches(input, pattern);
+
+bool enabled = true;
+
+foreach (Match match in matches)
+{
+	if (match.Value.Equals("don't()"))
+	{
+		enabled = false;
+		continue;
+	}
+	else if (match.Value.Equals("do()"))
+	{
+		enabled = true;
+		continue;
+	}
+
+	if (enabled)
+	{
+		long a = long.Parse(match.Groups[4].Value);
+		long b = long.Parse(match.Groups[5].Value);
+
+		results += a * b;
+	}
 }
 
 Console.WriteLine($"The results of the multiplications are {results}");
